0812ca5d8c7b850c6885b4ce070f97b6290437e4
Darren Cotton <dee_c1@yahoo.co.uk>
2021-10-18 13:51:57 +0100

    building graphql api pluralsight course module 4 complete

---
+++ schema.js
@@ -0,0 +1,42 @@
+
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+type Query {
+    sessions(
+        id: ID,
+        title: String
+        description: String
+        startsAt: String
+        endsAt: String
+        room: String
+        day: String
+        format: String
+        track: String
+        level: String
+    ): [Session]
+    sessionById(id:ID): Session
+    speakers: [Speaker]
+    speakerById(id: ID): Speaker
+}
+
+type Speaker {
+    id:ID!
+    bio: String
+    name: String
+    sessions: [Session]
+}
+
+type Session {
+    id: ID!
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String @deprecated(reason: "this is how we mark an API that is going to be removed.")
+    level: String
+    speakers: [Speaker]
+}`
